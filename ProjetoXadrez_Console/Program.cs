15db650ed4e2bf7ac5eae0e20625762fcd7bfa16
Everton Vicente <everton.vicente@onfriday.com.br>
2022-01-26 11:40:50 -0300

    Tratando erros com excessao personalizadas

--- ProjetoXadrez_Console/Program.cs
+++ ProjetoXadrez_Console/Program.cs
@@ -6,12 +6,19 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Tabuleiro tab = new Tabuleiro(8,8);
+        try
+        {
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-        tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-        tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-        tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
 
-        Tela.imprimirTabuleiro(tab);
+            Tela.imprimirTabuleiro(tab);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
