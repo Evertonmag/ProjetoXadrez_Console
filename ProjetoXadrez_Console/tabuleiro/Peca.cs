d17cc25cbb495d65ad79015cd866e8564ea76929
Everton Vicente <everton.vicente@onfriday.com.br>
2022-01-26 11:03:04 -0300

    comecando a colocar pecas no tabuleiro

--- ProjetoXadrez_Console/tabuleiro/Peca.cs
+++ ProjetoXadrez_Console/tabuleiro/Peca.cs
@@ -6,9 +6,9 @@ internal class Peca
     public int qteMovimentos { get; protected set; }
     public Tabuleiro tab { get; protected set; }
 
-    public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+    public Peca(Cor cor, Tabuleiro tab)
     {
-        this.posicao = posicao;
+        this.posicao = null;
         this.cor = cor;
         this.qteMovimentos = 0;
         this.tab = tab;
